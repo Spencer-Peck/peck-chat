532d7c3a5bdcc8dab2a2c3c79225410f2887b745
spencer <pec15002@byui.edu>
2019-03-11 16:08:42 -0600

    initial version

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function getConversationsFromDb(id, callback) {
 	var sql = " SELECT conversations.id AS conversation_id, messages.user_id AS last_message_owner_id, auth_user.messages_read_at < messages.created_at AS has_unread_messages, messages.id, messages.content, messages.created_at,";
 	sql += " messages.user_id = 7 AS mine, other_users.id, other_users.first_name, other_users.avatar_url FROM conversations INNER JOIN messages ON conversations.last_message_id = messages.id";
 	sql += " INNER JOIN participants other_participants ON other_participants.conversation_id = conversations.id AND other_participants.user_id != $1::int";
-	sql += " INNER JOIN users other_users ON other_participants.user_id = other_users.id AND other_users.id = messages.user_id INNER JOIN participants auth_user ON auth_user.conversation_id = conversations.id";
+	sql += " INNER JOIN users other_users ON other_participants.user_id = other_users.id INNER JOIN participants auth_user ON auth_user.conversation_id = conversations.id";
 	sql +=	" AND auth_user.user_id = $1::int ORDER BY messages.created_at DESC;";
 
 	var params = [id];
