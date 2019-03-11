fef2e00991c77a730a3664fb66b741b421d87d92
spencer <pec15002@byui.edu>
2019-03-11 17:28:16 -0600

    initial version

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/conversations', function(request, response) {
+app.get('/conversations/:id', function(request, response) {
 	getConversations(request, response);
 });
 
@@ -27,7 +27,7 @@ app.listen(port, function() {
 });
 
 function getConversations(request, response) {
-	var id = request.query.id;
+	var id = request.params.id;
 
 
 	getConversationsFromDb(id, function(error, result) {
