fc9b2a0fdd531e271ed86652f49e94e63f2fc067
spencer <pec15002@byui.edu>
2019-03-13 14:49:57 -0600

    initial version

--- public/server_client.js
+++ public/server_client.js
@@ -2,12 +2,12 @@ $(function(){
 	//make connection
 	//var socket = io.();
 	const PORT = process.env.PORT || 5000;
-	var socket = io.connect('https://lit-woodland-79911.herokuapp.com:' + PORT);
+	var socket = io.();
 
 	socket.on("update_conversations", (data) => {
 		//chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
 		$.get('/conversations', (data) => {
-			data.forEach(addMessages);
+			data.forEach();
 		});
 	});
 
