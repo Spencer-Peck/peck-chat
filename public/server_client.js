da6c4fa0395158bf32104fb56b834b7ad3442233
spencer <pec15002@byui.edu>
2019-03-13 15:06:43 -0600

    initial version

--- public/server_client.js
+++ public/server_client.js
@@ -1,7 +1,7 @@
 $(function(){
 	//make connection
 	//var socket = io.();
-	var socket = io('https://tranquil-rocky-mountain-92476.herokuapp.com/');
+	var socket = io.connect('https://tranquil-rocky-mountain-92476.herokuapp.com/');
 
 	socket.on("update_conversations", (data) => {
 		//chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
