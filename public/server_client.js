03c02f5bb2245a9aa72b33841c11fab56273b97d
spencer <pec15002@byui.edu>
2019-03-14 12:16:01 -0600

    initial version

--- public/server_client.js
+++ public/server_client.js
@@ -12,7 +12,7 @@ function addConversation(conversation){
 		conversation.avatar_url = "https://ptetutorials.com/images/user-profile.png";
 		console.log("avatar is null");
 	}
-   $("#inbox_chat").append('<div class="chat_list"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><h5>'+conversation.first_name+'<span class="chat_date"></span></h5><p>'+conversation.content+'</p></div></div></div>');
+   $("#inbox_chat").append('<div class="chat_list"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><h5>'+conversation.first_name+'<span class="chat_date">Dec 25</span></h5><p>'+conversation.content+'</p></div></div></div>');
    }
 
 function getConversations(){
