32cae3debddf81b8b9e694ec8a5b424e73c70628
spencer <pec15002@byui.edu>
2019-03-14 16:02:09 -0600

    initial version

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -1,4 +1,5 @@
 const conversationModels = require("../models/conversationModels.js");
+var dateFormat = require('dateformat');
 
 
 function getConversations(request, response) {
@@ -9,10 +10,19 @@ function getConversations(request, response) {
 	conversationModels.getConversationsFromDb(id, function(error, result) {
 
 
-		var conversations = result;
+		var conversations = JSON.parse(result);
+
+		$.each(conversations, function(i, item) {
+			item.created_at = dateFormat(item.created_at, "mmmm dS, dddd");
+		});
+
+
+
+
+
 		console.log(result);
 
-	    response.json(result);
+	    response.json();
 
 
 	});
--- public/server_client.js
+++ public/server_client.js
@@ -15,11 +15,10 @@ function addConversation(conversation){
 	}
 	//var dateFormat = require('dateformat');
 	//var date = dateFormat(conversation.created_at, "mmmm, dddd");
-	date = "";
+	date = conversation.created_at;
 
-
-   $("#inbox_chat").append('<div class="chat_list"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><div class="chat_ib"><h5>'+conversation.first_name+'<span class="chat_date">'+date+'</span></h5><p>'+conversation.content+'</p></div></div></div>');
-   }
+	$("#inbox_chat").append('<div class="chat_list"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><div class="chat_ib"><h5>'+conversation.first_name+'<span class="chat_date">'+date+'</span></h5><p>'+conversation.content+'</p></div></div></div>');
+}
 
 function getConversations(){
 	$.get('/conversations?id=9', function(data) {
