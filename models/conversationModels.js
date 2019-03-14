b368c12216de8972c2d7f2f37724650eb7a67ba1
spencer <pec15002@byui.edu>
2019-03-13 19:21:16 -0600

    initial version

--- models/conversationModels.js
+++ models/conversationModels.js
@@ -9,7 +9,7 @@ function getConversationsFromDb(id, callback) {
 	sql += " messages.user_id = $1::int AS mine, other_users.id, other_users.first_name, other_users.avatar_url FROM conversations INNER JOIN messages ON conversations.last_message_id = messages.id";
 	sql += " INNER JOIN participants other_participants ON other_participants.conversation_id = conversations.id AND other_participants.user_id != $1::int";
 	sql += " INNER JOIN users other_users ON other_participants.user_id = other_users.id INNER JOIN participants auth_user ON auth_user.conversation_id = conversations.id";
-	sql +=	" AND auth_user.user_id = $1::int ORDER BY messages.created_at DESC;";
+	sql +=	" AND auth_user.user_id = $1::int ORDER BY conversations.id, messages.created_at DESC;";
 
 	var params = [id];
 	pool.query(sql, params, function(err, result) {
