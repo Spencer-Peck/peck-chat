52f45d683df01efb85364288a644a93708223753
spencer <pec15002@byui.edu>
2019-03-14 20:50:09 -0600

    initial version

--- public/server_client.js
+++ public/server_client.js
@@ -22,6 +22,7 @@ function addConversation(conversation){
 		}
 		names = names.slice(0, -1);
 		console.log(names);
+		$("#inbox_chat").append('<div class="chat_list"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><div class="chat_ib"><h5>'+conversation.first_name+ " " + conversation.last_name + names +'<span class="chat_date">'+date+'</span></h5><p>'+conversation.content+'</p></div></div></div>');
 
 	});
 
@@ -29,7 +30,7 @@ function addConversation(conversation){
 	//var date = dateFormat(conversation.created_at, "mmmm, dddd");
 	date = "";
 
-	$("#inbox_chat").append('<div class="chat_list"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><div class="chat_ib"><h5>'+conversation.first_name+ " " + conversation.last_name+names+'<span class="chat_date">'+date+'</span></h5><p>'+conversation.content+'</p></div></div></div>');
+	
 }
 
 function getConversations(){
