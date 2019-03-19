854da3bb37e8a1d7db5daf09682ddd9ed42f5aee
spencer <pec15002@byui.edu>
2019-03-18 21:27:09 -0600

    initial version

--- public/server_client.js
+++ public/server_client.js
@@ -3,7 +3,7 @@
 var PORT;
 function setup(){
 	getConversations();
-	getMessages();
+	//getMessages();
 }
 
 
@@ -16,7 +16,7 @@ function addConversation(conversation){
 	}
 	date = "";
 	
-	$("#inbox_chat").append('<div class="chat_list"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><div class="chat_ib"><h5>'+conversation.first_name+ " " + conversation.last_name+'<span class="chat_date">'+date+'</span></h5><p>'+conversation.content+'</p></div></div></div>');
+	$("#inbox_chat").append('<div class="chat_list" onclick="addMessage('+conversation.conversation_id+')"><div class="chat_people"><div class="chat_img"> <img src="'+conversation.avatar_url+'" alt=""> </div><div class="chat_ib"><h5>'+conversation.first_name+ " " + conversation.last_name+'<span class="chat_date">'+date+'</span></h5><p>'+conversation.content+'</p></div></div></div>');
 
 
 }
