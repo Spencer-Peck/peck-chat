c313649f4f6ff8308367dc804abef3a45dd716a8
spencer <pec15002@byui.edu>
2019-03-19 15:18:33 -0600

    initial version

--- models/messageModels.js
+++ models/messageModels.js
@@ -6,8 +6,8 @@
 
   function getMessagesFromDb(id, con_id, callback) {
 	var sql = "SELECT DISTINCT messages.user_id = $1::int AS mine, users.first_name, messages.created_at, messages.content, users.avatar_url FROM conversations";
-    sql += " INNER JOIN participants ON participants.conversation_id = $2::int INNER JOIN users ON users.id = participants.user_id";
-	sql += " INNER JOIN messages ON messages.user_id = users.id ORDER BY messages.created_at;";
+    sql += " INNER JOIN participants ON participants.conversation_id = conversations.id AND conversations.id = $2::int INNER JOIN users ON users.id = participants.user_id";
+	sql += " INNER JOIN messages ON messages.user_id = users.id AND messages.conversation_id = conversations.id ORDER BY messages.created_at;";
 	var params = [id, con_id];
 	pool.query(sql, params, function(err, result) {
 
